d4ddf4aa856227fd43666909051d7a8f0611ef4c
alperenf <fatihbab34@gmail.com>
2022-10-22 03:12:23 +0300

    Added min/max range for frames

--- auto-output.py
+++ auto-output.py
@@ -9,6 +9,11 @@ file_field.value = ""
 frame_folder_field = mset.UITextField()
 frame_folder_field.value = ""
 
+frame_range_min_field = mset.UITextField()
+frame_range_min_field.value = ""
+frame_range_max_field = mset.UITextField()
+frame_range_max_field.value = ""
+
 def set_displacement_map():
     path = mset.showOpenFileDialog(["tif"])
     if path != '':
@@ -49,13 +54,20 @@ def set_frame_folder():
  
 def set_material_frames():
     frameRootPath = frame_folder_field.value #"c:\\frame_6-8-9-10" # ex. C:\Users\enemy\Desktop\maps\house\Houset_Edit\Frame_6-8-9-10
-    material_range: tuple = (0, 30)
+    material_range: tuple = (int(frame_range_min_field.value), int(frame_range_max_field.value)) if (frame_range_min_field.value != "" and frame_range_min_field != "" ) else (0, 0)
+    print(material_range)
     materials = mset.getSelectedMaterialGroup()
+    if materials is None:
+        print("Syserr: No materials selected")
+        return
+    
     materials_count = len(materials)
+    material_idx = int(frame_range_min_field.value) if frame_range_min_field.value != "" else 0
     for material in materials:
-        material_nummer = material.name
+        material_nummer = material_idx + int(material.name) if (frame_range_min_field.value != "" and frame_range_max_field.value != "" ) else material.name
         
-        #if not material_range[0] <= int(material_nummer) <= material_range[1]:
+        print("material_nummer: {} frame folder nummer: {}".format(material.name, material_nummer))
+        #if (frame_range_min_field.value != "" and frame_range_min_field != "") and (not material_range[0] <= int(material_nummer) <= material_range[1]):
         #    continue
         
         rootFrameFolderName = os.path.basename(os.path.normpath(frameRootPath))
@@ -63,14 +75,12 @@ def set_material_frames():
         currentFrameFolderName = rootFrameFolderName + "_" + material_number_new_format
         currentFrameName = material_number_new_format + ".tif"
         currentFramePath = os.path.normpath(os.path.join(frameRootPath, currentFrameFolderName, currentFrameName)).replace(os.sep, "/")
-        print(currentFramePath)
         displacement = material.getSubroutine("displacement")
         if displacement is None:
             material.setSubroutine("displacement", "Height")
             displacement = material.getSubroutine("displacement")
         
         displacement.setField("Displacement Map", currentFramePath)
-        #displacement.setField("Displacement Map", "C:/tests/frames/frame_4_5_6/frame_4_5_6_000/000.tif")
             
 # Main UI
 window = mset.UIWindow(name="Auto Material to BakeOutput")
@@ -97,6 +107,11 @@ window.addElement(frame_folder_field)
 frame_folder_button = mset.UIButton("...")
 frame_folder_button.onClick = set_frame_folder
 window.addElement(frame_folder_button)
+
+window.addElement(mset.UILabel("Min range"))
+window.addElement(frame_range_min_field)
+window.addElement(mset.UILabel("Max"))
+window.addElement(frame_range_max_field)
 
 copy_material_frames_button = mset.UIButton("Copy Material Frames")
 copy_material_frames_button.onClick = set_material_frames
